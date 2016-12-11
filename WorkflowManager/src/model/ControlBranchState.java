c211e6041dfd6889216c70db1672a41baae15176
jmc637 <kohwoojin@gmail.com>
2016-12-11 00:44:38 -0500

    Update ControlBranchState.java

--- WorkflowManager/src/model/ControlBranchState.java
+++ WorkflowManager/src/model/ControlBranchState.java
@@ -1,11 +1,47 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ControlBranchState extends State {
+	//INHERITED
+	//form required to move to next state
+	//FormType requiredForm;
 
-	//Allows parrallel branching to other states. State that keeps track of all states that branch off of it.
-    ArrayList<State> NextStates;
-    void IncreaseTokensAndMove();
+    //unique id
+	//String name_id;
+    
+	//boolean to denote whether this is start state
+	//boolean isStartState;
+	
+	//gets all the required forms
+    //ArrayList<FormType> getRequiredForms(){
+    //	return this.requiredForms;
+    //}
+	
+    //create form for this state
+    //gets all the required forms
+//	Form createFormForThisState(){
+//		List<Field> fields = new ArrayList<Field>();
+//		for(Entry<String, String> entry : this.requiredForm.FormFields.entrySet()){
+//			Field newField = new Field(entry.getKey(), null, entry.getValue());
+//			fields.add(newField);
+//		}
+//		return new Form(fields);
+//	}
+	
+	//Multiple next states
+	List<State> nextStates;
+	
+	public ControlBranchState(String nameid, FormType requiredForm, boolean isStartState){
+		this.name_id = nameid;
+		this.requiredForm = requiredForm;
+		this.isStartState = isStartState;
+	}
+	
+	public void setNextStates(List<State> nextStates){
+		this.nextStates = nextStates;
+	}
 	
 }
+
