e87de14a36878eabe32027ec624714c6bf9ce6a8
jmc637 <kohwoojin@gmail.com>
2016-12-11 00:49:18 -0500

    Update WorkflowInstance.java

--- WorkflowManager/src/model/WorkflowInstance.java
+++ WorkflowManager/src/model/WorkflowInstance.java
@@ -5,26 +5,25 @@ import java.util.List;
 
 public class WorkflowInstance {
 
-    private List<State> currentStates;
-    private int workflowID;
-    private List<Form> forms;
+    private List<Token> allTokens;
     private WorkflowStructure structure;
 
     public WorkflowInstance(WorkflowStructure structure, int workflowID) {
-        this.structure = structure;
-        this.currentStates.add(this.structure.StartState);
-        forms = new ArrayList<>();
-        currentStates.forEach(state -> forms.addAll(state.getForms()));
-        this.workflowID = workflowID;
+       this.structure = structure;
+       this.allTokens = new ArrayList<Token>();
+       //TODO put token in start state for workflow
+       
+       
+    }
+    
+    //TODO implement this method that will get all tokens in a said state by state name
+    public List<Token> getTokensInState(String stateID){
+    	return null;
+    }
+    
+    //TODO implement this method to get all token in a state by actual state instance
+    public List<Token> getTokensInState(State state){
+    	return null;
     }
 
-    public void move(State src, State dst) {
-        currentStates.remove(src);
-        currentStates.add(dst);
-        forms.addAll(dst.getForms());
-    }
-
-    public List<State> getCurrentStates() {
-        return currentStates;
-    }
 }
