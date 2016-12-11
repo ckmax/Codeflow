82e5c8f9a4bc21581da0b338bfd5f84268f1d195
jmc637 <kohwoojin@gmail.com>
2016-12-11 00:46:36 -0500

    Update FormType.java

--- WorkflowManager/src/model/FormType.java
+++ WorkflowManager/src/model/FormType.java
@@ -1,10 +1,14 @@
 package model;
 
+import java.util.HashMap;
+
 public class FormType {
 	
 	//Below is hashmap required to store the form fields of a form
     //The key string is the name of that field
     //The value related to key is the field type (Eg. Integer input, date, string, etc.)
-    Hashmap<String, String> FormFields;
-
+    HashMap<String, String> FormFields;
+    
+    //TODO write Constructor
+    
 }
